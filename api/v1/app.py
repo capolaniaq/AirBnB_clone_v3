f76ee4b63e44273e55ee8873c25652966e30ccdc
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-09-20 19:36:35 -0500

    pep8 api/v1/app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,7 +7,6 @@ from os import environ
 from api.v1.views import app_views
 
 
-
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
@@ -17,10 +16,10 @@ def close_db(error):
     """ Close Storage """
     storage.close()
 
+
 @app.errorhandler(404)
 def page_not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
-
 
 if __name__ == "__main__":
     """ Main Function """
